0b6070d469276309eaa2578a021507459591ad0f
${BATpindao} <${1483258276@qq.com}>
2021-06-24 16:10:53 +0800

    text分支此一修改

---
+++ TextDamo/src/com/TextBranch.java
@@ -0,0 +1,8 @@
+package com;
+
+public class TextBranch {
+    public static void main(String[] args) {
+        System.out.println("git branch:创建分支 git branch 分支名字（ch:尺）");
+        System.out.println("git checkout:切换分支 git chackout 名字");
+    }
+}
