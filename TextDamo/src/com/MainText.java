37de17a51d3c2f99e3c05cd7253d074c01dce715
${BATpindao} <${1483258276@qq.com}>
2021-06-24 15:33:32 +0800

    这是第五次尝试更改

--- TextDamo/src/com/MainText.java
+++ TextDamo/src/com/MainText.java
@@ -3,5 +3,6 @@ package com;
 public class MainText {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("这是IDEA2020.3版本的，回退提交的方式");
     }
 }
