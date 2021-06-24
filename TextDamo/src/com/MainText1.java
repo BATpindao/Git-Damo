de5e2c5c8fe0eb158d10dd2e221871118ccaf4b9
${BATpindao} <${1483258276@qq.com}>
2021-06-24 17:10:46 +0800

    text分支第二次修改

--- TextDamo/src/com/MainText1.java
+++ TextDamo/src/com/MainText1.java
@@ -3,6 +3,6 @@ package com;
 public class MainText1 {
     public static void main(String[] args) {
         System.out.println("我好像有点明白了");
-        System.out.println("github ");
+        System.out.println("github");
     }
 }
--- TextDamo/src/com/TextBranch.java
+++ TextDamo/src/com/TextBranch.java
@@ -4,6 +4,6 @@ public class TextBranch {
     public static void main(String[] args) {
         System.out.println("git branch:创建分支 git branch 分支名字（ch:尺）");
         System.out.println("git checkout:切换分支 git checkout 名字");
-        System.out.println("查看前分支 git checkout 切换分支 git merge 合并分支");
+        System.out.println("git branch创建分支，查看前分支 git checkout 切换分支 git merge 合并分支");
     }
 }
