fedbdb4a1d14e6119e45d79c93317295bb6422d4
${BATpindao} <${1483258276@qq.com}>
2021-06-24 16:18:58 +0800

    text分支第一次修改

--- TextDamo/src/com/TextBranch.java
+++ TextDamo/src/com/TextBranch.java
@@ -3,6 +3,6 @@ package com;
 public class TextBranch {
     public static void main(String[] args) {
         System.out.println("git branch:创建分支 git branch 分支名字（ch:尺）");
-        System.out.println("git checkout:切换分支 git chackout 名字");
+        System.out.println("git checkout:切换分支 git checkout 名字");
     }
 }
